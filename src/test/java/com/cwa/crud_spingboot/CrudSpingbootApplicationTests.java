c5d4ae77e48b57a2976e0a4088b4bc678102194f
Landry Dupont <landrydupont790@gmail.com>
2025-03-19 10:54:25 +0100

    first commit

---
+++ src/test/java/com/cwa/crud_spingboot/CrudSpingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwa.crud_spingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpingbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
